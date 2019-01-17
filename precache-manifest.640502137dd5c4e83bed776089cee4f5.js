9b12f8c60534dcbe542b18ed0111ce725201d598
Joan Mira <jmira@sapient.com>
2019-01-17 13:00:16 +0100

    Updates

---
+++ precache-manifest.640502137dd5c4e83bed776089cee4f5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "93927df25a890736a87503229747911f",
+    "url": "/static/media/otras_1.93927df2.jpg"
+  },
+  {
+    "revision": "1efc135f8e709ae30530",
+    "url": "/static/css/main.09312b68.chunk.css"
+  },
+  {
+    "revision": "fa7bbb380f9c48785b6e",
+    "url": "/static/js/1.fa7bbb38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd7f3dadb2867ee0184c0bf1feb6ee7d",
+    "url": "/static/media/favicon.dd7f3dad.ico"
+  },
+  {
+    "revision": "e1916698385f531a7b9ea08a62f0e819",
+    "url": "/static/media/home.e1916698.jpg"
+  },
+  {
+    "revision": "1efc135f8e709ae30530",
+    "url": "/static/js/main.1efc135f.chunk.js"
+  },
+  {
+    "revision": "ed89cbe8f92b2fd2f5bad2a2ed0e9b42",
+    "url": "/static/media/otras_2.ed89cbe8.jpg"
+  },
+  {
+    "revision": "2e576ea38a7cecbfccebb57065865dea",
+    "url": "/static/media/otras_3.2e576ea3.jpg"
+  },
+  {
+    "revision": "9c5a79ed182e1a0045c8497fc044d75d",
+    "url": "/static/media/expo-ravi-cafe.9c5a79ed.jpg"
+  },
+  {
+    "revision": "c72777d49291d6414435528261357f8b",
+    "url": "/static/media/expo-blauart.c72777d4.jpg"
+  },
+  {
+    "revision": "caa2a8c8bcd1efef88f09102230b0e47",
+    "url": "/index.html"
+  }
+];
